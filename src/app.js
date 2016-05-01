51af67b714df7cf29b62e1c16a38793444362b1c
gokulkrishh <krishnangokul9@gmail.com>
2016-05-01 19:35:37 +0530

    Added touch event to close menu

--- src/app.js
+++ src/app.js
@@ -1,24 +1,79 @@
-var $menuIcon = document.querySelector(".menu-icon");
-var $menuContainer = document.querySelector(".menu-container");
-var $menuOverlay = document.querySelector(".menu-overlay");
+(function () {
+  "use strict";
 
-function toggleMenu() {
-  var menuStyle = $menuContainer.style.transform;
-  var overlayStyle = $menuOverlay.style.transform;
-  if (menuStyle === "translateX(-103%)" || menuStyle === "") {
-    $menuContainer.style.transform = "translateX(0)";
-    $menuOverlay.style.transform = "translateX(0)";
+  var $menuIcon = document.querySelector(".menu__icon");
+  var $menu = document.querySelector(".menu");
+  var $menuOverlay = document.querySelector(".menu__overlay");
+
+  addEventListeners();
+
+  function addEventListeners() {
+    // document.addEventListener("transitionend", transitionEnd);
+    document.addEventListener("touchstart", touchStart, false);
+    document.addEventListener("touchmove", touchMove, false);
+    document.addEventListener("touchend", touchEnd, false);
+    $menuIcon.addEventListener("click", toggleMenu, false);
+    $menuOverlay.addEventListener("click", toggleMenu, false);
   }
-  else {
-    $menuContainer.style.transform = "translateX(-103%)";
-    $menuOverlay.style.transform = "translateX(-103%)";
+
+  function toggleMenu() {
+    if (!isMenuOpen()) {
+      showMenu();
+    }
+    else {
+      hideMenu();
+    }
   }
-}
 
-$menuIcon.addEventListener("click", function () {
-  toggleMenu();
-}, false);
+  function isMenuOpen() {
+    return $menu.classList.contains("menu--show");
+  }
 
-$menuOverlay.addEventListener("click", function () {
-  toggleMenu();
-}, false);
+  function hideMenu() {
+    $menu.style.transform = "";
+    $menu.classList.remove("menu--show");
+    $menuOverlay.classList.remove("menu__overlay--show");
+  }
+
+  function showMenu() {
+    $menu.classList.add("menu--show");
+    $menuOverlay.classList.add("menu__overlay--show");
+  }
+
+  var startDistance, movedDistance = 0;
+  var distance = 0;
+
+  function transitionEnd() {
+    if (!isMenuOpen()) {
+      hideMenu();
+    }
+  }
+  
+  function touchStart(event) {
+    if (!isMenuOpen()) {
+      return;
+    }
+    startDistance = event.touches[0].pageX;
+    distance = startDistance;
+  }
+
+  function touchMove(event) {
+    if (!isMenuOpen()) {
+      return;
+    }
+    movedDistance = event.touches[0].pageX;
+    var translate = Math.min(0, movedDistance - startDistance);
+    if (translate < 0) {
+      event.preventDefault();
+    }
+    $menu.style.transform = "translateX("+ translate +"px)";
+  }
+
+  function touchEnd(event) {
+    var endDistance = Math.min(0, movedDistance - startDistance);
+    $menu.style.transform = "";
+    if (endDistance < 0) {
+      hideMenu();
+    }
+  }
+})();
